907bff81e9d29e6a8cba9ba0ca3bf167b7a6cfbe
c5huracan <93015898+c5huracan@users.noreply.github.com>
2025-01-18 11:24:33 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+document.addEventListener('DOMContentLoaded', (event) => {
+    const hamburger = document.querySelector('.hamburger-menu');
+    const navLinks = document.querySelector('.nav-links');
+
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+        hamburger.classList.toggle('active');
+    });
+});
